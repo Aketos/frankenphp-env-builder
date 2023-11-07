b0426c9ff4eff35bfd9269d9146d701c83f29a7a
Aketos <lagardemathieu@hotmail.fr>
2023-11-07 14:54:12 +0100

    docs: add comments

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 cd $APP_NAME
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+  # Install runtime/frankenphp-symfony as dev dependency if not present
   if [ -f "composer.json" ]; then
-      # Utilise jq pour rechercher le package dans composer.json
       if ! jq -e ".require_dev.\"runtime/frankenphp-symfony\"" "composer.json" > /dev/null 2>&1; then
            composer require --dev runtime/frankenphp-symfony
            composer config --json extra.symfony.docker 'true'
@@ -14,26 +14,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
       exit 1
   fi
 
-	# Install the project the first time PHP is started
-	# After the installation, the following block can be deleted
-#	if [ ! -f composer.json ]; then
-#		rm -Rf tmp/
-#		composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
-#
-#		cd tmp
-#		cp -Rp . ..
-#		cd -
-#		rm -Rf tmp/
-#
-#		composer require "php:>=$PHP_VERSION" runtime/frankenphp-symfony
-#		composer config --json extra.symfony.docker 'true'
-#
-#		if grep -q ^DATABASE_URL= .env; then
-#			echo "To finish the installation please press Ctrl+C to stop Docker Compose and run: docker compose up --build -d --wait"
-#			sleep infinity
-#		fi
-#	fi
-
+  # Install vendors if not already done
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
